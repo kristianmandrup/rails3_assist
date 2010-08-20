1c06dfc4755a6d31b8ca33a50eed1b5047aca659
Kristian Mandrup <kmandrup@gmail.com>
2010-08-20 15:44:47 +0200

    added more error handling to #existing_file_name

--- lib/rails3_assist/base/file_name.rb
+++ lib/rails3_assist/base/file_name.rb
@@ -9,14 +9,25 @@ module Rails::Assist
 
       def existing_file_name name, type=nil 
         # first try finder method
-        finder_method = :"find_#{type}"
-        found_file = send finder_method, name if respond_to?(finder_method)
-        return found_file if found_file
-        
-        raise "Existing migration file: #{name} can't be found!" if type == :migration
-          
+        finder_method = :"find_#{type}"       
+        if respond_to?(finder_method)          
+          found_file = send finder_method, name 
+          return found_file if found_file
+          raise "The call to #finder_#{type}(#{name}) didn't find an existing #{type} file"
+        end        
+
+        raise "The method #finder_#{type} to find the migration is not available!" if type == :migration          
+
         # default for non-migration
         make_file_name(name, type)      
+      end
+
+      Rails::Assist.artifacts.each do |name|
+        class_eval %{
+          def existing_#{name}_file name, type=nil
+            existing_file_name name, type
+          end
+        } 
       end
     end
     
