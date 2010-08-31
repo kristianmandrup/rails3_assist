1597a66cda4ba1bc6973e780a2587b09b46011a9
Kristian Mandrup <kmandrup@gmail.com>
2010-08-31 17:16:29 +0200

    using include and extend pattern for methods which are both instance and class methods

--- lib/rails3_assist/directory/app.rb
+++ lib/rails3_assist/directory/app.rb
@@ -1,20 +1,17 @@
 module Rails3::Assist::Directory
   module App  
-    def self.app_directories
-      Rails3::Assist::Artifact::app_artifacts.map {|a| a.to_s.pluralize.to_sym}
-    end
+    module Methods
+      def app_directories
+        Rails3::Assist::Artifact::app_artifacts.map {|a| a.to_s.pluralize.to_sym}
+      end
 
-    def app_directories
-      Rails3::Assist::Directory::App.app_directories
-    end
-
-    def self.app_dir options={}
-      rails_root_dir = Rails3::Assist::Directory::Root.root_dir(options)
-      File.join(rails_root_dir, 'app')
+      def app_dir options={}
+        rails_root_dir = Rails3::Assist::Directory::Root.root_dir(options)
+        File.join(rails_root_dir, 'app')
+      end
     end
     
-    def app_dir options={}
-      Rails3::Assist::Directory::App.app_dir options
-    end
+    include Methods
+    extend Methods
   end
 end
--- lib/rails3_assist/directory/container.rb
+++ lib/rails3_assist/directory/container.rb
@@ -1,17 +1,24 @@
 module Rails3::Assist::Directory      
   module Container  
+
     # return list of symbols for all kinds of supported Rails 3 containers 
     # (those that have a [container]_dir method)
-    def self.rails_containers
-      app_methods       = Rails3::Assist::Directory.instance_methods
-      artifact_methods  = Rails3::Assist::Artifact::Directory.instance_methods
-      all_methods = app_methods + artifact_methods
     
-      all_methods.grep(/_dir$/).map{|dir| dir.to_s.gsub(/^(.*)_dir/, '\1').to_sym }
-    end
+    module Methods    
+      def rails_containers
+        app_methods       = Rails3::Assist::Directory.instance_methods
+        artifact_methods  = Rails3::Assist::Artifact::Directory.instance_methods
+        all_methods = app_methods + artifact_methods
+    
+        all_methods.grep(/_dir$/).map{|dir| dir.to_s.gsub(/^(.*)_dir/, '\1').to_sym }
+      end
 
-    def self.valid_container? type
-      rails_containers.include?(type)
+      def valid_container? type
+        rails_containers.include?(type)
+      end
     end
+    
+    include Methods
+    extend Methods
   end
 end
