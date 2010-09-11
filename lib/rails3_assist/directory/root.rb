269644783d6d0278de746ff13a00f99dce1d053a
Kristian Mandrup <kmandrup@gmail.com>
2010-09-11 17:12:17 +0200

    root_dir fix

--- lib/rails3_assist/directory/root.rb
+++ lib/rails3_assist/directory/root.rb
@@ -7,7 +7,7 @@ module Rails3::Assist::Directory
 
       def root_dir options={}
         raise ArgumentError, "options argument to root_dir must be a hash, was: #{options.inspect}" if options && !options.kind_of?(Hash)
-        dir = options[:root_path] 
+        dir = options[:root_path] if options 
         dir ||= Rails3::Assist::Directory.rails_root || Rails.root
         raise "You must set the Rails app root dir: Rails::Assist::App.root_dir = '/my/root/dir'" if !dir
         dir
