599a512c6de3e56dc9eab4729ef76dc980b03459
Kristian Mandrup <kmandrup@gmail.com>
2010-09-14 16:27:32 +0200

    improved some more

--- lib/rails3_assist/file.rb
+++ lib/rails3_assist/file.rb
@@ -24,12 +24,14 @@ module Rails3::Assist
           end  
 
           def create_#{name} name, &block
+            Rails3::Assist.create_empty_tmp #{name} if !::File.directory?(Rails3::Assist::Artifact::Directory.#{name}_dir)
             ::File.overwrite #{name}_file(name) do
               yield
             end
           end
 
           def remove_all_#{plural_name}
+            return if !::File.directory?(Rails3::Assist::Artifact::Directory.#{name}_dir)
             #{name}_files.each{|f| ::File.delete_file! f}
           end
 
@@ -37,7 +39,8 @@ module Rails3::Assist
             return remove_all_#{plural_name} if names.empty? 
             names.to_strings.each do |name|
               name = (name =~ /.rb$/) ? name : "\#{name}.#{pure_ext}"
-              ::File.delete #{name}_file(name)
+              file_name = #{name}_file(name)
+              ::File.delete file_name if ::File.file?(file_name)
             end
           end
           alias_method :remove_#{name}, :remove_#{plural_name} 
